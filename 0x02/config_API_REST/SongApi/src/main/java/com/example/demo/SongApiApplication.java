8bd71d602c69d543242989d1450d07cc410e32b2
KarinaMCorreia <ka.mendes.correia@gmail.com>
2025-11-29 18:06:19 -0300

    feat: criar projeto Spring Boot SongApi e configurar porta 8081 e contexto /api-song

--- 0x02/config_API_REST/SongApi/src/main/java/com/example/demo/SongApiApplication.java
+++ 0x02/config_API_REST/SongApi/src/main/java/com/example/demo/SongApiApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SongApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SongApiApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SongApiApplication.class, args);
+	}
+
 }
